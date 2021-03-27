e56dac2c2435b604285454b0a7d99b19a9dfdeda
EL JARRAÏ <eljarrai.reda@gmail.com>
2021-03-27 13:27:14 +0100

    add unit tests for generating NullPointerException when args are missing

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -25,4 +25,31 @@ class AppTest
         Assertions.assertThrows(NullPointerException.class, () -> App.main(args));
 
     }
+
+    @Test
+    void the_Method_Main_should_throw_NullPointerException_when_input_argument_is_missing()
+    {
+
+        //GIVEN
+        String[] args = new String[2];
+        args[1] = "path/to/fileOutput.txt";
+
+        //WHEN
+        //THEN
+        Assertions.assertThrows(NullPointerException.class, () -> App.main(args));
+
+    }
+
+    @Test
+    void the_Method_Main_should_throw_NullPointerException_when_args_is_empty()
+    {
+        //GIVEN
+        String[] args = {};
+
+        //WHEN
+        //THEN
+        Assertions.assertThrows(NullPointerException.class, () -> App.main(args));
+    }
+
+
 }
